926e32a3908e518165eaffaaa87065b0f6d2b558
Magnus Hedlund <vindarmagnus@gmail.com>
2013-11-29 19:52:00 +0100

    Refactored calls to PdCalc, E_M_calc and Ten_cr_calc and changed inputs to structs. Tested running the code, and it seems to work.

--- E_M_calc.m
+++ E_M_calc.m
@@ -1,37 +1,37 @@
-function [ E, M ] = E_M_calc( idat )
+function [ E, M ] = E_M_calc(e)
 %E_W_CALC System energy and weight calculator for Spin2Win
 
 % Extract values from idat
-m  = idat(8,5);
-ri = (idat(1,1:1:m));
-ro = (idat(2,1:1:m));
-p  = idat(6,1:1:m);
-h  = (idat(8,2));
-n  = idat(8,3);
-n_ = idat(8,4);
+% e.m  = idat(8,5);
+% e.ri = (idat(1,1:1:e.m));
+% e.ro = (idat(2,1:1:e.m));
+% e.p  = idat(6,1:1:e.m);
+% e.h  = (idat(8,2));
+% e.n  = idat(8,3);
+% e.n_ = idat(8,4);
 
 % Preallocating vectors
 E = zeros(1,2);
 % E(1) = total energy, E(2) = Energy in rpm interval
-M = zeros(1,m+1);
+M = zeros(1,e.m+1);
 % W(1-m) induvidual shell weight, W(m+1) Total weigth [kg]
-I = zeros(1,m);
+I = zeros(1,e.m);
 
 % Useful relations
-w = 2*pi*n/60;                   % rpm to rad/sec conversion
-w_ = 2*pi*n_/60;                 % rpm to rad/sec conversion
+w = 2*pi*e.n/60;                   % rpm to rad/sec conversion
+w_ = 2*pi*e.n_/60;                 % rpm to rad/sec conversion
 
 % Shell mass calculation
-for k=1:1:m
-M(k) = h*p(k)*pi*(ro(k)^2-ri(k)^2);
+for k=1:1:e.m
+M(k) = e.h*e.p(k)*pi*(e.ro(k)^2-e.ri(k)^2);
 end
 
 % Calcualte total mass
-M(m+1) = sum(M);
+M(e.m+1) = sum(M);
 
 % Shell inertia calculation
-for k=1:1:m
-I(k) = 0.5*M(k)*(ro(k)^2+ri(k)^2);
+for k=1:1:e.m
+I(k) = 0.5*M(k)*(e.ro(k)^2+e.ri(k)^2);
 end
 
 % Energy calculation
---
+++ Figure_data_extract.m
@@ -0,0 +1,48 @@
+% Figure data extractor for subplotted figures
+% custom made to extract Spin2Win figure data.
+
+% get open figure handle number
+hand = gcf;
+
+% Get all data
+alldat = findall(hand,'Type','line');
+
+% Get all x-data
+x_all=get(alldat,'Xdata');
+
+% Get all y-data
+y_all=get(alldat,'YData');
+
+% Find double locations
+i = 1;
+for k=[1:length(x_all)]
+tmp = x_all(k,:);
+tmp = cell2mat(tmp);
+if length(tmp)>100
+    x_spot(i)=k;
+    i=i+1;
+end
+end
+
+i = 1;
+y_spot = 0;
+for k=[1:length(y_all)]
+tmp = y_all(k,:);
+tmp = cell2mat(tmp);
+if length(tmp)>100
+    y_spot(i)=k;
+    i=i+1;
+end
+end
+
+% Extract plot data
+for k=[1:length(x_spot)]
+x_cell(1,k) = (x_all(x_spot(k),:));
+x_dat(k,:)=cell2mat(x_cell(k));
+end
+
+for k=[1:length(y_spot)]
+y_cell(1,k) = (y_all(y_spot(k),:));
+y_dat(k,:)=cell2mat(y_cell(k));
+end
+
