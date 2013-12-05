ab8c74546b4105d2d1114107274f6ab264fd0688
Magnus Hedlund <vindarmagnus@gmail.com>
2013-12-05 22:48:26 +0100

    reformatted spin2win a bit. reweriting merge_indata

--- merge_indata.m
+++ merge_indata.m
@@ -1,4 +1,4 @@
-function [ senddat ] = merge_indata( geomdat, setdat )
+function [ senddat,d ] = merge_indata( geomdat, setdat )
 % Syntax:
 % [Output matrix] =
 % merge_indata(Geometrical data, Settings data)
@@ -7,21 +7,22 @@ function [ senddat ] = merge_indata( geomdat, setdat )
 % for easy transport between subfunctions
 
 % Pre allocate matrix
-senddat = zeros(10,length(geomdat(1,:)));
+n = length(geomdat(1,:));
+senddat = zeros(10,n);
 
 % Copy geometrical data into output matrix
 for i=1:1:7
-    for k=1:1:length(geomdat(1,:))
+    for k=1:1:n
     senddat(i,k) = geomdat(i,k);
     end
 end
 
-for k=1:1:length(geomdat(1,:))
-senddat(9,k) = geomdat(8,k);
+for k=1:1:n
+    senddat(9,k) = geomdat(8,k);
 end
 
-for k=1:1:length(geomdat(1,:))
-senddat(10,k) = geomdat(9,k);
+for k=1:1:n
+    senddat(10,k) = geomdat(9,k);
 end
 
 
@@ -30,6 +31,7 @@ i = 8;
 for k=2:1:4
     senddat(i,k) = setdat(k,1);
 end
+d.m = setdat(8,1);
 
 % Insert number of active shells into output matrix
 senddat(i,5) = setdat(1,1);
