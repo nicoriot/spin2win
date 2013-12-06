c1115a1e281f00df94261f85b0ce2b2d7a1d1223
Tobias K. <kamfen@gmail.com>
2013-12-06 17:17:22 +0100

    ez_calc mods, still broken

--- ez_calc.m
+++ ez_calc.m
@@ -14,9 +14,7 @@ disp('*** *** ***  RUNNING EZ_CALC  *** *** ***');
 % Preallocating vectors
 Ten_c   = zeros(1,e.m-1);
 Ten_r   = zeros(1,e.m-1);
-%e_z     = zeros(1,e.m);
-e_c      = zeros(1,e.m);
-e_r      = zeros(1,e.m);
+Ten_z   = zeros(1,e.m-1);
 u       = zeros(1,e.m);
 Pi      = zeros(1,e.m-1);
 Po      = zeros(1,e.m-1);
@@ -59,23 +57,28 @@ v_zc(k) = v_rc(k);
 v_cz(k) = e.v_cr(k);
 
 % sweep e_z over range, calcualte energy
-e_z =(-0.02:0.00001:0.02);
-SE = zeros(1,length(e_z));
+e_z      =(-0.02:0.00001:0.02);
+SE       = zeros(1,length(e_z));
+e_c      = zeros(1,length(e_z));
+e_r      = zeros(1,length(e_z));
 
 
-for i = 1:1:length(e_z)
+for i = 1:1:length(e_z) % e_z sweep loop
     
-% Make Young's modulus quotas and b quota
+% note: k still used as index despite that e_z calc only use one k at a
+% time. This is a prepiration for if many k wants to be run in the samme
+% loop.
+    
+% Make Young's modulus quotas, a and b quota
 u(k) = sqrt((e.Ec(k)/e.Er(k))*((1-v_rz(k)*v_zr(k))/(1-v_zc(k)*v_cz(k)))); % my
 b(k) = (e.v_cr(k)+v_cz(k)*v_zr(k))/(1-v_zc(k)*v_cz(k)); % beta
 a(k) = e.Ec(k)*((e_z(i)*(v_zc(k)-v_zr(k)))/(1-v_zc(k)*v_cz(k))); %alpha
 
 
 % Make C1 C2 and Q constants using eq 2.28 and 2.29 from theory chapter 
-    
+% Complimented C1 and C2 from thesis compliment    
 Q(k) = (e.p(k)*w^2*(3+b(k))/(u(k)^2-9)); %last part of 2.20
 Q2(k) = e.p(k)*w^2*(u(k)^2+3*b(k))/(u(k)^2-9); % last part of 2.22
-
 D(k) = a(k)/(u(k)^2-1); % D from thesis compliment.
 
 C1(k) = (((e.ri(k)*e.ro(k))^(u(k)))/(e.ri(k)^(2*u(k))-e.ro(k)^(2*u(k))))...
@@ -86,11 +89,13 @@ C2(k) = (Q(k)*(e.ro(k)^(u(k)+3)-e.ri(k)^(u(k)+3))+(Po(k)-D(k))*e.ro(k)^(u(k)+1)+
         (D(k)-Pi(k))*e.ri(k)^(u(k)+1))/(e.ri(k)^(2*u(k))-e.ro(k)^(2*u(k)));
 
     
-% Calc shell k outer strains
-Ten_r(k) = C1(k)*e.ri(k)^(-1-u(k))+C2(k)*e.ri(k)^(-1+u(k))+Q(k)*e.ri(k)^2-D(k);
+% Calc shell stress and strains
+Ten_r(k) = C1(k)*e.ri(k)^(-1-u(k))+C2(k)*e.ri(k)^(-1+u(k))+Q(k)*e.ri(k)^2-D(k)
     
 Ten_c(k) = u(k)*(C2(k)*e.ri(k)^(-1+u(k))-C1(k)*e.ri(k)^(-1-u(k)))...
-           +Q2(k)*e.ri(k)^2-D(k);
+           +Q2(k)*e.ri(k)^2-D(k)
+
+Ten_z(k) = e.Er(k)*(e_z(i)+v_rz(k)*Ten_r(k)/e.Er(k)+v_cz(k)*Ten_c(k)/e.Ec(k))
     
 e_c(i) = (Ten_c(k)/e.Ec(k))*(1-v_zc(k)*v_cz(k))...
          -(Ten_r(k)/e.Er(k))*(v_rc(k)+v_zc(k)*v_rz(k))-v_zc(k)*e_z(i);
@@ -98,18 +103,28 @@ e_c(i) = (Ten_c(k)/e.Ec(k))*(1-v_zc(k)*v_cz(k))...
 e_r(i) = (Ten_r(k)/e.Er(k))*(1-v_zr(k)*v_rz(k))...
          -(Ten_c(k)/e.Ec(k))*(e.v_cr(k)+v_zr(k)*v_cz(k))-v_zr(k)*e_z(i); 
         
- 
  % Stress, strain calc done
- % Now calc energy
  
- %SE(i) = (1/(2*(1+v_rz(k))))*(e.Ec(k)*e_c(i)^2+e.Er(k)*e_r(i)^2+...
- %       e.Er(k)*e_z(i)^2+v_rz(k)*(e_c(i)+e_r(i)+e_z(i))/(1-2*v_rz(k))); 
-  SE(i) = (1/2)*(e.Ec(k)*e_c(i)^2+e.Er(k)*e_r(i)^2+e.Er(k)*e_z(i)^2)...
-          +e.ri(k)^2*w^2*e.p(k)*e_c(i);
+ % Calc energy for current strain. Correct????
+ % SE(i) = (1/2)*(e.Ec(k)*e_c(i)^2+e.Er(k)*e_r(i)^2+e.Er(k)*e_z(i)^2)...
+ %         +e.ri(k)^2*w^2*e.p(k)*e_c(i);
+     
+  SE(i) = (1/2)*(Ten_c(k)*e_c(i)+Ten_r(k)*e_r(i)+Ten_z(k)*e_z(i))...
+         -e.ri(k)^2*w^2*e.p(k)*e_c(i);
    
 end % e_z loop end
   
-  [~,N] = min(SE); % find min energy in interbval
-  e_zmin = -e_z(N); % what is min energy's e_z WHY -e_z????? but works
+  %%% HAXXPLOT rememeber to remove
+  figure(5)
+  plot(e_z,SE,'-b')
+  hold on
+  %%% HAXXPLOT
+
+  [~,N] = min(SE); % find min energy in interval
+  e_zmin = -e_z(N); % what is min energy's e_z, 
+  plot(e_z(N),SE(N),'*r')
+  
+  %WHY -e_z????? It works for iso materials but breaks for ortho.
+  %Check energy formula.
 
 end % function end
